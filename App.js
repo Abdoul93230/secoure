f2f72643abe49dd3a2b4d1a85bd8049558333335
Abdoul_Razak <abdoulrazak9323@gmail.com>
2024-10-28 03:14:22 -0700

    callback2

--- App.js
+++ App.js
@@ -28,7 +28,8 @@ const io = socketIo(server, {
       "https://habou227-seller.onrender.com",
       "https://e-habou.onrender.com",
       "https://ihambaobab.onrender.com",
-      " http://localhost:5500",
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
     ],
     credentials: true,
   },
@@ -53,6 +54,7 @@ app
         "https://e-habou.onrender.com",
         "https://ihambaobab.onrender.com",
         "http://localhost:5500",
+        "http://127.0.0.1:5500",
       ],
       credentials: true,
     })
